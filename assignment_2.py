7df3aeaa4de75b49982bf7eeae0222e73afa2771
Martin Perez <maperezrodriguez602@alamancecc.edu>
2024-08-31 18:32:52 -0400

    Create assignment_2.py

---
+++ assignment_2.py
@@ -0,0 +1,41 @@
+# Please answer each question and ensure each is answered as throughou as possible.
+# There is no minimum word requirment or maximum. Comprehension is key.
+
+## --> TODO: Question 1
+#What is virtual environment? How are these virtual environments used in programming and why are they important?
+
+## --> TODO: Question 2
+#What is difference between the configurations of a virtual environment and the configs of your global machine?
+
+## --> TODO: Question 3
+#What is versión control in programming? Describe the process which is followed?
+
+## --> TODO: Question 4
+#Describe what the following commands do:
+# <git init>
+# <git remote add origin>
+# <git pull origin main>
+# <git add (for names)>
+# <git commit -m “comment here”<
+# <git checkout>
+# <git status>
+# <git remote -v>
+# <git branch -a>
+
+## --> TODO: Question 5
+#What are ssh keys? Why are these important and describe the process of creating these on your local machine?
+
+## --> TODO: Question 6
+#What is terminal/command prompt used for? Why may these be important to engineers?
+
+## --> TODO: Question 7
+# Describe what these linux commands do:
+# <ls>
+# <cd>
+# <mkdir>
+# <touch>
+# <cd>
+# <cp>
+# <mv>
+# <cat>
+# <tail>
